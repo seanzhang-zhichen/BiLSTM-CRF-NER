3cdd542effd308188872c0d71b737298c6917e36
张志晨 <zhangzhichen@zhangzhichen.local>
2022-04-07 21:14:41 +0800

    modify param test

--- src/main.py
+++ src/main.py
@@ -5,10 +5,10 @@ from tools.get_ner_level_acc import precision
 if __name__ == "__main__":
 
         use_pretrained_w2v = False
-        model_type = "bilstm-crf"
+        model_type = "bilstm"
         
         model_train = Train()
-        # model_train.train(use_pretrained_w2v=use_pretrained_w2v,  model_type=model_type)
+        model_train.train(use_pretrained_w2v=use_pretrained_w2v,  model_type=model_type)
         
         text = "张铁柱，大学本科，毕业于东华理工大学，汉族，江西抚州人"
 
